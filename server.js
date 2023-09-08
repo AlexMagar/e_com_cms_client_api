4fe401e7370ba57fd6302cc1619ce98b2c2e8817
AlexMagar <lx.mgr07@gmail.com>
2023-09-08 22:44:11 +1000

    server added

---
+++ server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+const PORT = process.env.PORT || 8000
+
+const app = express();
+
+app.use(express.json())
+app.use(cors())
+app.use(morgan('dev'))
+
+//apis
+app.use("api/v1/admin", adminRouter)
+
+// default route
+app.get("/", (req, res) =>{
+    res.json({
+        status: "success",
+        message: "server is running well"
+    })
+})
+
+//for error handling code
+app.use((error, req, res, next) =>{
+    const code = error.statusCode || 500;
+    res.status(code).json({
+        status: "error",
+        message: error.message
+    })
+})
+
+// PORT Listen
+app.listen(PORT, (err) =>{
+    console.log(err)
+    err
+    ? console.log(err.message)
+    : console.log(`server is running at http://localhost:${PORT}`)
+})
