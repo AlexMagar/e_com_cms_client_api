f907770bdb906f999073f65d4d8a489f7741f5b7
AlexMagar <lx.mgr07@gmail.com>
2023-09-19 23:50:36 +1000

    authentication and authorization via jwt

---
+++ src/utils/jwt.js
@@ -0,0 +1,30 @@
+
+
+export const createAccessJWT = async (email) =>{
+    const token = jwt.sign({email}, process.env.JWT_ACCESS_SECRET, {
+        expiresIn: "15m"
+    })
+    console.log("JWT createAccessToken", token)
+    await insertSesstion({token, associate: email})
+
+    return token
+}
+
+export const verifyAccessJWT = (token) => {
+    return jwt.verify(token, process.env.JWT_ACCESS_SECRET)
+}
+
+export const createRefreshJWT = async ( email) => {
+    const refreshJWT = jwt.sign({email}, process.env.JWT_REFRESH_SECRET, {
+        expiresIn: "30d"
+    })
+
+    console.log("JWT verification: ", refreshJWT)
+    const dt = await updateAdmin({email}, { refreshJWT})
+
+    return refreshJWT
+}
+
+export const verifyRefreshJWT = (token) => {
+    return jwt.verify(token, process.env.JWT_REFRESH_SECRET)
+}
